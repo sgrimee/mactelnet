874bbbd83701788941455e6a7a82b7bbb96e5fef
sgrimee <sgrimee@gmail.com.me>
2023-09-07 10:17:42 +0200

    defaultSystems

--- flake.nix
+++ flake.nix
@@ -2,38 +2,45 @@
   description = "MAC-Telnet for Posix systems";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
+    flake-utils,
     nixpkgs,
-  }: {
-    defaultPackage.aarch64-darwin = with import nixpkgs {system = "aarch64-darwin";};
-      stdenv.mkDerivation {
-        name = "mactelnet";
-        src = fetchFromGitHub {
-          owner = "haakonnessjoen";
-          repo = "MAC-Telnet";
-          rev = "c3dc4515b1aff09372cdb04aef393437dc2d8f60";
-          sha256 = "sha256-EKs0x5YsmmVD/2Qrqzi3/OXHQc5VSPs+aJa7cLHtAD8=";
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = with pkgs; rec {
+        mactelnet = stdenv.mkDerivation {
+          name = "mactelnet";
+          src = fetchFromGitHub {
+            owner = "haakonnessjoen";
+            repo = "MAC-Telnet";
+            rev = "c3dc4515b1aff09372cdb04aef393437dc2d8f60";
+            sha256 = "sha256-EKs0x5YsmmVD/2Qrqzi3/OXHQc5VSPs+aJa7cLHtAD8=";
+          };
+          nativeBuildInputs = [
+            autoconf
+            automake
+            autoreconfHook
+            gettext
+            libtool
+            openssl
+            pkg-config
+          ];
+          installPhase = ''
+            cd src
+            mkdir -p $out/bin;
+            install -t $out/bin mactelnet macping mndp;
+            mkdir -p $out/sbin;
+            install -t $out/sbin mactelnetd;
+          '';
         };
-        nativeBuildInputs = [
-          autoconf
-          automake
-          autoreconfHook
-          gettext
-          libtool
-          openssl
-          pkg-config
-        ];
-        installPhase = ''
-          cd src
-          mkdir -p $out/bin;
-          install -t $out/bin mactelnet macping mndp;
-          mkdir -p $out/sbin;
-          install -t $out/sbin mactelnetd;
-        '';
+        default = mactelnet;
       };
-  };
+    });
 }
